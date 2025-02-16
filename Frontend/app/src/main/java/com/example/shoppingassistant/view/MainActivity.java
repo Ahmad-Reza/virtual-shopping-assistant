19ceeba9e871dc2df781aee2b62c83ca0c68a0dc
Ahmad <areza.dev@gmail.com>
2025-02-17 03:34:15 +0530

    Improve the user interface for VSA

--- Frontend/app/src/main/java/com/example/shoppingassistant/view/MainActivity.java
+++ Frontend/app/src/main/java/com/example/shoppingassistant/view/MainActivity.java
@@ -1,17 +1,17 @@
 package com.example.shoppingassistant.view;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.shoppingassistant.R;
 import com.example.shoppingassistant.model.AssistantResponse;
-import com.example.shoppingassistant.model.ProductSearchModel;
 import com.example.shoppingassistant.model.UserQuery;
 import com.example.shoppingassistant.network.ApiService;
 import com.example.shoppingassistant.network.RetrofitClient;
@@ -29,13 +29,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        userInput = findViewById(R.id.user_input);
-        responseText = findViewById(R.id.response_text);
-        Button askButton = findViewById(R.id.ask_button);
+        userInput = findViewById(R.id.input_message);
+        responseText = findViewById(R.id.tv_ai_message);
+        ImageButton askButton = findViewById(R.id.send_button);
 
         askButton.setOnClickListener(v -> {
             String query = userInput.getText().toString();
-//            searchProducts(query);
             askShoppingAssistant(query);
         });
     }
@@ -47,40 +46,20 @@ public class MainActivity extends AppCompatActivity {
         Call<AssistantResponse> call = apiService.askAssistant(userQuery);
         call.enqueue(new Callback<AssistantResponse>() {
             @Override
-            public void onResponse(Call<AssistantResponse> call, Response<AssistantResponse> response) {
+            public void onResponse(@NonNull Call<AssistantResponse> call, @NonNull Response<AssistantResponse> response) {
                 if (response.isSuccessful() && response.body() != null) {
                     String assistantReply = response.body().getResponse();
+                    responseText.setText(assistantReply);
                     Toast.makeText(MainActivity.this, assistantReply, Toast.LENGTH_LONG).show();
                 } else {
+                    responseText.setText(response.message());
                     Toast.makeText(MainActivity.this, "Error: " + response.message(), Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
-            public void onFailure(Call<AssistantResponse> call, Throwable t) {
+            public void onFailure(@NonNull Call<AssistantResponse> call, Throwable t) {
                 Toast.makeText(MainActivity.this, "API Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-
-    private void searchProducts(String query) {
-        ApiService apiService = RetrofitClient.getInstance();
-
-        // Fetch products
-        apiService.searchProducts("Floral Skirt", 40, "S").enqueue(new Callback<ProductSearchModel>() {
-            @Override
-            public void onResponse(Call<ProductSearchModel> call, Response<ProductSearchModel> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    // for (Product product : response.body()) {
-                    //   Log.d("API Response", "Product: " + product.getName() + ", Price: $" + product.getPrice());
-                    //}
-                }
-            }
-
-            @Override
-            public void onFailure(Call<ProductSearchModel> call, Throwable t) {
-                Log.e("API Error", t.getMessage());
             }
         });
     }
